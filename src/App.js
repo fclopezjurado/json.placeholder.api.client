10f92f59ffe21b233f33570d630dbfe44042b5b9
Fran López <fclopezjurado@gmail.com>
2018-09-20 14:30:54 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import MainLayout from "./components/MainLayout";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <MainLayout autoWidth={false} width="100%" listStyle={{width: '0.01%'}} style={{width: '100%'}}/>
       </div>
     );
   }
